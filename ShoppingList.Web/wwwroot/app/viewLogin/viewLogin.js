c93e0e6338356e742da8912d978e2dbbbfed27f1
Mikael Strid <mikael.strid@outlook.com>
2016-05-29 22:22:16 +0200

    Definierade Home och Login-vyerna.

---
+++ ShoppingList.Web/wwwroot/app/viewLogin/viewLogin.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+angular.module('myApp.viewLogin', ['ngRoute'])
+
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/login', {
+        templateUrl: 'app/viewLogin/viewLogin.html',
+        controller: 'LoginCtrl'
+    });
+}])
+
+.controller('LoginCtrl', [function () {
+
+}]);
---
+++ ShoppingList.Web/wwwroot/app/viewLogin/viewLogin_test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe('myApp.viewLogin module', function () {
+
+    beforeEach(module('myApp.viewLogin'));
+
+    describe('login controller', function () {
+
+        it('should ....', inject(function ($controller) {
+            var loginCtrl = $controller('LoginCtrl');
+            expect(loginCtrl).toBeDefined();
+        }));
+
+    });
+});
