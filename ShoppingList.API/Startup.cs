e4875336d2a30e8bab63f283703b32e6f50888e9
Mikael Strid <mikael.strid@outlook.com>
2016-05-31 22:14:31 +0200

    Skapade ett "resurs-API" och kopplade ihop det med autentisering.

---
+++ ShoppingList.API/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Http;
+using Lily.ShoppingList.API;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace Lily.ShoppingList.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
