0cd42ed6143cf141a2762c4c631520e894176aca
Mikael Strid <mikael.strid@outlook.com>
2016-09-29 06:32:25 +0200

    Dolde progressbar om position inte kunde bestämmas.

--- Web/wwwroot/app/commute/components/publicTransport/publicTransportController.js
+++ Web/wwwroot/app/commute/components/publicTransport/publicTransportController.js
@@ -5,35 +5,45 @@
         .module('myApp.commute.publicTransport')
         .controller('PublicTransportCtrl', controller);
 
-    controller.$inject = ['$log', '$location', '$mdDialog', '$mdToast', '$geolocation', 'vasttrafikService', 'profileService'];
+    controller.$inject = ['$scope', '$log', '$location', '$mdDialog', '$mdToast', '$geolocation', 'vasttrafikService', 'profileService'];
 
-    function controller($log, $location, $mdDialog, $mdToast, $geolocation, vasttrafikService, profileService) {
+    function controller($scope, $log, $location, $mdDialog, $mdToast, $geolocation, vasttrafikService, profileService) {
         var vm = this;
         vm.isLoading = false;
+        vm.ErrorMessage = '';
         vm.upcomingPublicTransportTrips = [];
         vm.lastUpdateTime = null;
 
         vm.getUpcomingPublicTransportTrips = function() {
             vm.isLoading = true;
+            vm.errorMessage = '';
             var me = profileService.me;
             $geolocation.getCurrentPosition({
-                timeout: 60000
-            }).then(function (position) {
-                vm.lastUpdateTime = null;
-                vm.upcomingPublicTransportTrips = [];
-                vasttrafikService.getUpcomingTrips(position.coords, me.homeLocation, me.workLocation, me.homePublicTransportStationId, me.workPublicTransportStationId)
-                    .then(
-                        function(trips) {
-                            vm.upcomingPublicTransportTrips = trips;
-                            vm.lastUpdateTime = new Date();
-                        },
-                        function (reason) { showError(reason + ' :(', 'vasttrafikService.upcomingPublicTransportTrips', null); }
-                    )
-                    .finally(function () { vm.isLoading = false; });
-            }, function () {
-                showError('Kunde inte bestämma din position :(', '$geolocation.getCurrentPosition', null);
-                vm.isWorking = false;
-            });
+                    timeout: 60000
+                })
+                .then(
+                    function(position) {
+                        vm.lastUpdateTime = null;
+                        vm.upcomingPublicTransportTrips = [];
+                        vasttrafikService.getUpcomingTrips(position.coords,
+                                me.homeLocation,
+                                me.workLocation,
+                                me.homePublicTransportStationId,
+                                me.workPublicTransportStationId)
+                            .then(
+                                function(trips) {
+                                    vm.upcomingPublicTransportTrips = trips;
+                                    vm.lastUpdateTime = new Date();
+                                },
+                                function(reason) {
+                                    showError(reason + ' :(', 'vasttrafikService.upcomingPublicTransportTrips', null);
+                                }
+                            );
+                    },
+                    function() {
+                        showError('Kunde inte bestämma din position :(', '$geolocation.getCurrentPosition', null);
+                    })
+                .finally(function () { vm.isLoading = false; });
         }
 
         vm.refresh = function() {
@@ -45,6 +55,7 @@
         function showError(messageToUser, failedMethodName, error) {
             if (!error || error.status !== 401) {
                 $mdToast.show($mdToast.simple().textContent(messageToUser).hideDelay(3000));
+                vm.errorMessage = messageToUser;
             }
             $log.log('Call to ' + failedMethodName + ' failed: ' + error.statusText);
         }
