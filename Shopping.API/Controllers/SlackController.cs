95bc29af8a7ee691b9e6958eb86421fb3244b370
Mikael Strid <mikael.strid@outlook.com>
2016-09-30 22:33:50 +0200

    Authorize-filter för Slack samt utkast på SlackController.

---
+++ Shopping.API/Controllers/SlackController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http;
+using Lilybot.Shopping.Infrastructure;
+
+namespace Lilybot.Shopping.API.Controllers
+{
+    [Route("api/slack")]
+    [SlackTokenAuthorize]
+    public class SlackController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] SlackCommand cmd)
+        {
+            return Ok();
+        }
+    }
+
+    public class SlackCommand
+    {
+        // ReSharper disable InconsistentNaming
+        public string token { get; set; }
+        public string team_id { get; set; }
+        public string team_domain { get; set; }
+        public string channel_id { get; set; }
+        public string channel_name { get; set; }
+        public string user_id { get; set; }
+        public string user_name { get; set; }
+        public string command { get; set; }
+        public string text { get; set; }
+        public string response_url { get; set; }
+    }
+}
---
+++ Shopping.Infrastructure/SlackAuthorizeFilter.cs
@@ -0,0 +1,38 @@
+﻿using System.Configuration;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace Lilybot.Shopping.Infrastructure
+{
+    public class SlackTokenAuthorizeAttribute : AuthorizeAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if (AuthorizeRequest(actionContext)) return;
+
+            HandleUnauthorizedRequest(actionContext);
+        }
+
+        protected override void HandleUnauthorizedRequest(HttpActionContext actionContext)
+        {
+            throw new HttpResponseException(HttpStatusCode.Unauthorized);
+        }
+
+        private bool AuthorizeRequest(HttpActionContext actionContext)
+        {
+            try
+            {
+                var content = actionContext.Request.Content.ReadAsStringAsync().Result;
+                var actualToken = HttpUtility.ParseQueryString(content)["token"];
+                var expectedToken = ConfigurationManager.AppSettings["SlackToken"];
+                return actualToken == expectedToken;
+            }
+            catch
+            {
+                throw new HttpResponseException(HttpStatusCode.Unauthorized);
+            }
+        }
+    }
+}
