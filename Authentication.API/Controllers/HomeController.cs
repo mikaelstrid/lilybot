69e2dfeb5ff3e96087a69ee5c99fc68dd53e2ac2
Mikael Strid <mikael.strid@outlook.com>
2016-10-02 21:36:09 +0200

    Lade till controller för att returnera OK/200.

---
+++ Authentication.API/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+
+namespace Lilybot.Authentication.API.Controllers
+{
+    [RoutePrefix("")]
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
