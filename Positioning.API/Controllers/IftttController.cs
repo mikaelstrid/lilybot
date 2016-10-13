4a875f3817a97bbd4884a2858f536ef005d04080
Mikael Strid <mikael.strid@outlook.com>
2016-10-13 19:04:52 +0200

    Skapade nya projekt för Positioning samt uppdaterade några nuget-paket.

---
+++ Positioning.API/Controllers/IftttController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using Lilybot.Positioning.Infrastructure;
+
+namespace Lilybot.Positioning.API.Controllers
+{
+    [RoutePrefix("api/ifttt")]
+    public class IftttController : ApiController
+    {
+        [Route("hotspot/enter")]
+        [HttpPost]
+        [BodyApiKeyAuthorize]
+        public IHttpActionResult PostHotspotEnter([FromBody] HotspotEnterApiModel model)
+        {
+            return Ok();
+        }
+    }
+
+    public class HotspotEnterApiModel
+    {
+    }
+}
---
+++ Positioning.Infrastructure/BodyApiKeyAuthorizeAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using Newtonsoft.Json.Linq;
+
+namespace Lilybot.Positioning.Infrastructure
+{
+    public class BodyApiKeyAuthorizeAttribute : AuthorizeAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if (AuthorizeRequest(actionContext)) return;
+
+            HandleUnauthorizedRequest(actionContext);
+        }
+
+        protected override void HandleUnauthorizedRequest(HttpActionContext actionContext)
+        {
+            throw new HttpResponseException(HttpStatusCode.Unauthorized);
+        }
+
+        private static bool AuthorizeRequest(HttpActionContext actionContext)
+        {
+            try
+            {
+                var httpBody = actionContext.Request.Content.ReadAsStringAsync().Result;
+                dynamic dynamicBody = JObject.Parse(httpBody);
+                var actualApiKey = (string) dynamicBody.apiKey;
+                var allowedApiKeys = ConfigurationManager.AppSettings["ApiKeys"].Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries).Select(t => t.Trim());
+                return allowedApiKeys.Contains(actualApiKey);
+            }
+            catch
+            {
+                throw new HttpResponseException(HttpStatusCode.Unauthorized);
+            }
+        }
+    }
+}
